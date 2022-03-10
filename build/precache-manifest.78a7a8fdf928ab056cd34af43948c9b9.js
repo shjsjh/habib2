2689c6f9d2ce1719fee6aa2f6d76a79c37846193
webdev0202 <webdev0202@yandex.com>
2022-03-10 07:51:25 +0100

    new commit

---
+++ build/precache-manifest.78a7a8fdf928ab056cd34af43948c9b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8254ec84804c2664e639483f25e6e07f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2304734cddbb4a7f844",
+    "url": "/static/css/main.70867bac.chunk.css"
+  },
+  {
+    "revision": "7620bd7f89688db4e7e6",
+    "url": "/static/js/2.4d0c642c.chunk.js"
+  },
+  {
+    "revision": "f2304734cddbb4a7f844",
+    "url": "/static/js/main.f256c416.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8a66e29d3b4f54f44ae979627a6ee045",
+    "url": "/static/media/logoo.8a66e29d.PNG"
+  },
+  {
+    "revision": "5049d9b0544259cad1aa5c520b4c2aca",
+    "url": "/static/media/sunset.5049d9b0.jpg"
+  }
+]);
